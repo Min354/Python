083d3fa5febe90f7b9fafe44e9e65a0174580039
Min354 <meanwestk@gmail.com>
2024-03-10 01:23:19 +0900

    Add files via upload

---
+++ 기본편/02/1자료형.py
@@ -0,0 +1,17 @@
+#숫자 자료형
+print('\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n')
+print(3+(3*67))
+print(3.14)
+
+#문자 자료형
+print('\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n')
+print('string')
+
+#boolean 자료형
+print('\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n')
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not (5>10))
---
+++ 기본편/02/2변수.py
@@ -0,0 +1,10 @@
+x = '2'
+print('1' + x + '3')
+
+x = 2
+print(x + 2)
+print(str(x) + '-')
+
+isTrue = x > 3
+print(str(isTrue) + '-')
+print(x, isTrue, 'str')     # , 으로 str() 없이 출력 시 변수 사이에 공백 존재 
